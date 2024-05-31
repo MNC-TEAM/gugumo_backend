fd3ece788db671b41226ccd9cd6e038d6605c6ff
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-06-01 04:01:24 +0900

    알림 관련 repository 작성
    - NotificationRepository
    - EmitterRepository

---
+++ src/main/java/sideproject/gugumo/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package sideproject.gugumo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sideproject.gugumo.domain.entity.Notification;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
