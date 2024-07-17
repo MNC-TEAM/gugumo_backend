4426e0943fddb344f0b9d4815c0e23cec8f611da
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-07-18 04:45:37 +0900

    PostController 설명 작성

--- src/main/java/sideproject/gugumo/api/controller/PostController.java
+++ src/main/java/sideproject/gugumo/api/controller/PostController.java
@@ -1,5 +1,7 @@
 package sideproject.gugumo.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -28,12 +30,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/meeting")
+@Tag(name = "Post")
 public class PostController {
 
     private final PostService postService;
 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "글쓰기", description = "게시글을 작성합니다.")
     public ApiResponse<String> save(@AuthenticationPrincipal CustomUserDetails principal,
                                     @RequestBody @Valid CreatePostReq createPostReq) {
         postService.save(principal, createPostReq);
@@ -47,32 +51,35 @@ public class PostController {
      * 동적 정렬 기능이 필요하면 스프링 데이터 페이징이 제공하는 Sort를 사용하기 보다는 파라미터를 받아서 직접 처리하는 것을 권장한다.
      */
     @GetMapping
+    @Operation(summary = "게시글 조회", description = "주어진 조건에 맞는 게시글을 조회합니다.")
     public <T extends SimplePostDto> ApiResponse<PageCustom<T>> findPostSimple(
             @AuthenticationPrincipal CustomUserDetails principal,
-            @PageableDefault(size=12) Pageable pageable,
+            @PageableDefault(size = 12) Pageable pageable,
             @RequestParam(required = false, value = "q") String q,
-            @RequestParam(required = false, value="location") String location,
-            @RequestParam(required = false, value="gametype") String gameType,
-            @RequestParam(required = false, value="meetingstatus", defaultValue = "RECRUIT") String meetingStatus,
+            @RequestParam(required = false, value = "location") String location,
+            @RequestParam(required = false, value = "gametype") String gameType,
+            @RequestParam(required = false, value = "meetingstatus", defaultValue = "RECRUIT") String meetingStatus,
             @RequestParam(required = false, value = "sort", defaultValue = "NEW") String sortType) {
-
 
 
         return ApiResponse.createSuccess(postService.findSimplePost(principal, pageable, q, gameType, location, meetingStatus, sortType));
     }
+
     @GetMapping("/{post_id}")
+    @Operation(summary = "게시글 상세 조회", description = "게시글의 상세 정보를 조회합니다.")
     public <T extends DetailPostDto> ApiResponse<T> findPostDetail(
             @AuthenticationPrincipal CustomUserDetails principal,
             @PathVariable("post_id") Long postId) {
         DetailPostDto detailPostDto = postService.findDetailPostByPostId(principal, postId);
 
-        return ApiResponse.createSuccess((T)detailPostDto);
+        return ApiResponse.createSuccess((T) detailPostDto);
     }
 
     @PatchMapping("/{post_id}")
+    @Operation(summary = "게시글 수정", description = "게시글을 수정합니다.")
     public ApiResponse<String> updatePost(@AuthenticationPrincipal CustomUserDetails principal,
-                                             @PathVariable("post_id") Long postId,
-                                             @RequestBody @Valid UpdatePostReq updatePostReq) {
+                                          @PathVariable("post_id") Long postId,
+                                          @RequestBody @Valid UpdatePostReq updatePostReq) {
         postService.update(principal, postId, updatePostReq);
 
         return ApiResponse.createSuccess("글 갱신 완료");
@@ -80,6 +87,7 @@ public class PostController {
 
 
     @DeleteMapping("/{post_id}")
+    @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
     public ApiResponse<String> deletePost(
             @AuthenticationPrincipal CustomUserDetails principal,
             @PathVariable("post_id") Long postId) {
@@ -91,21 +99,21 @@ public class PostController {
 
 
     @GetMapping("/my")
+    @Operation(summary = "내 게시글 조회", description = "내가 작성한 게시글을 조회합니다.")
     public <T extends SimplePostDto> ApiResponse<PageCustom<T>> findMyPost(
             @AuthenticationPrincipal CustomUserDetails principal,
-            @PageableDefault(size=12, sort="createDate", direction = Sort.Direction.DESC) Pageable pageable,
+            @PageableDefault(size = 12, sort = "createDate", direction = Sort.Direction.DESC) Pageable pageable,
             @RequestParam(required = false, value = "q", defaultValue = "") String q) {
         return ApiResponse.createSuccess(postService.findMyPost(principal, pageable, q));
 
     }
 
     @GetMapping("/recommend")
+    @Operation(summary = "추천 게시글 조회", description = "선호 종목에 맞춘 추천 게시글을 조회합니다.")
     public <T extends SimplePostDto> ApiResponse<List<T>> findRecommendPost(
             @AuthenticationPrincipal CustomUserDetails principal) {
         return ApiResponse.createSuccess(postService.findRecommendPost(principal));
     }
-
-
 
 
 }
