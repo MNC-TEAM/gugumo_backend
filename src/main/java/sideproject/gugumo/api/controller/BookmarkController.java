b50d0cf51ef12f7a8e13ebdd595a3b670e5fe58d
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-07-28 03:42:56 +0900

    북마크 등록 openapi 작성

--- src/main/java/sideproject/gugumo/api/controller/BookmarkController.java
+++ src/main/java/sideproject/gugumo/api/controller/BookmarkController.java
@@ -1,6 +1,10 @@
 package sideproject.gugumo.api.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -26,6 +30,34 @@ public class BookmarkController {
 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "북마크 등록", description = "북마크를 등록합니다.",
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "북마크 생성 완료",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"success\", \"data\" : \"북마크 생성 완료\", \"message\" : null}"
+                                    ))),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "북마크 중복 등록",
+                            content = @Content(schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"fail\", \"data\" : null, \"message\" : \"북마크 등록 실패: 이미 등록된 북마크입니다.\"}"
+                                    ))),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "비로그인 사용자",
+                            content = @Content(schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"fail\", \"data\" : null, \"message\" : \"북마크 등록 실패: 비로그인 사용자입니다.\"}"
+                                    ))),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "북마크 등록 권한 없음",
+                            content = @Content(schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"fail\", \"data\" : null, \"message\" : \"북마크 등록 실패: 권한이 없습니다.\"}"
+                                    ))),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "게시글이 존재하지 않음",
+                            content = @Content(schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"fail\", \"data\" : null, \"message\" : \"북마크 등록 실패: 해당 게시글이 존재하지 않습니다.\"}"
+                                    )))
+            })
     public ApiResponse<String> saveBookmark(
             @AuthenticationPrincipal CustomUserDetails principal,
             @RequestBody @Valid CreateBookmarkReq createBookmarkReq) {
--- src/main/java/sideproject/gugumo/request/CreateBookmarkReq.java
+++ src/main/java/sideproject/gugumo/request/CreateBookmarkReq.java
@@ -1,6 +1,7 @@
 package sideproject.gugumo.request;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -8,5 +9,6 @@ import lombok.Getter;
 public class CreateBookmarkReq {
 
     @NotNull
+    @Schema(description = "북마크할 게시글의 고유 번호")
     private Long postId;
 }
