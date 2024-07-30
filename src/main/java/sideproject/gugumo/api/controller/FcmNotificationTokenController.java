27822387d10ba9957233a9a9ca75879bdfffa362
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-07-30 23:50:35 +0900

    토큰 저장 openapi 작성

--- src/main/java/sideproject/gugumo/api/controller/FcmNotificationTokenController.java
+++ src/main/java/sideproject/gugumo/api/controller/FcmNotificationTokenController.java
@@ -1,5 +1,9 @@
 package sideproject.gugumo.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,6 +24,24 @@ public class FcmNotificationTokenController {
     private final FcmNotificationTokenService fcmNotificationTokenService;
 
     @PostMapping("/subscribe")
+    @Operation(summary = "FCM 토큰 저장 및 갱신", description = "FCM 토큰을 저장 및 갱신합니다.",
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "토큰 저장 완료",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"success\", \"data\" : \"북마크 생성 완료\", \"message\" : null}"
+                                    ))),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "비로그인 사용자",
+                            content = @Content(schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"fail\", \"data\" : null, \"message\" : \"토큰 저장 실패: 비로그인 사용자입니다.\"}"
+                                    ))),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "북마크 등록 권한 없음",
+                            content = @Content(schema = @Schema(implementation = ApiResponse.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"status\" : \"fail\", \"data\" : null, \"message\" : \"토큰 저장 실패: 권한이 없습니다.\"}"
+                                    )))
+            })
     public ApiResponse<String> subscribe(@AuthenticationPrincipal CustomUserDetails principal,
                                          @Valid @RequestBody FcmTokenDto fcmTokenDto) {
 
