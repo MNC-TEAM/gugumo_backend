7ea79f829ee650dbfeeb519538f6e29722d09556
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-09-21 23:10:14 +0900

    BookmarkApi 컨트롤러와 분리

--- src/main/java/sideproject/gugumo/api/swaggerapi/BookmarkApi.java
+++ src/main/java/sideproject/gugumo/api/swaggerapi/BookmarkApi.java
@@ -1,39 +1,26 @@
-package sideproject.gugumo.api.controller;
-
+package sideproject.gugumo.api.swaggerapi;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import sideproject.gugumo.api.swaggerapi.BookmarkApi;
 import sideproject.gugumo.domain.dto.memberDto.CustomUserDetails;
 import sideproject.gugumo.domain.dto.simplepostdto.SimplePostDto;
 import sideproject.gugumo.page.PageCustom;
 import sideproject.gugumo.request.CreateBookmarkReq;
 import sideproject.gugumo.response.ApiResponse;
-import sideproject.gugumo.service.BookmarkService;
 import sideproject.gugumo.swagger.simplepostdtoresponse.SimplePostLongDtoResponse;
 import sideproject.gugumo.swagger.simplepostdtoresponse.SimplePostShortDtoResponse;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/bookmark")
-@Tag(name = "Bookmark")
-public class BookmarkController implements BookmarkApi {
-
-    private final BookmarkService bookmarkService;
-
-
+public interface BookmarkApi {
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "북마크 등록", description = "북마크를 등록합니다.",
@@ -67,12 +54,7 @@ public class BookmarkController implements BookmarkApi {
             })
     public ApiResponse<String> saveBookmark(
             @AuthenticationPrincipal CustomUserDetails principal,
-            @RequestBody @Valid CreateBookmarkReq createBookmarkReq) {
-
-        bookmarkService.save(principal, createBookmarkReq);
-
-        return ApiResponse.createSuccess("북마크 생성 완료");
-    }
+            @RequestBody @Valid CreateBookmarkReq createBookmarkReq);
 
     @GetMapping
     @Operation(summary = "북마크 조회", description = "북마크한 게시글을 조회합니다.",
@@ -122,11 +104,8 @@ public class BookmarkController implements BookmarkApi {
             })
     public <T extends SimplePostDto> ApiResponse<PageCustom<T>> findBookmark(
             @AuthenticationPrincipal CustomUserDetails principal,
-            @PageableDefault(size = 12, sort="id", direction = Sort.Direction.DESC) Pageable pageable,
-            @RequestParam(required = false, value = "q", defaultValue = "") @Parameter(description = "검색어") String q) {
-
-        return ApiResponse.createSuccess(bookmarkService.findBookmarkByMember(principal, pageable, q));
-    }
+            @PageableDefault(size = 12, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            @RequestParam(required = false, value = "q", defaultValue = "") @Parameter(description = "검색어") String q);
 
     @DeleteMapping("/{bookmark_id}")
     @Operation(summary = "북마크 삭제", description = "북마크를 삭제합니다.",
@@ -159,11 +138,5 @@ public class BookmarkController implements BookmarkApi {
             })
     public ApiResponse<String> deleteBookmark(
             @AuthenticationPrincipal CustomUserDetails principal,
-            @PathVariable("bookmark_id") @Parameter(description = "삭제할 북마크 고유번호") Long bookmarkId) {
-
-        bookmarkService.delete(bookmarkId, principal);
-
-        return ApiResponse.createSuccess("북마크 삭제 완료");
-    }
-
+            @PathVariable("bookmark_id") @Parameter(description = "삭제할 북마크 고유번호") Long bookmarkId);
 }
